5d6f54f1536a318640577acee30aaf28c0933b55
OlegHarbar <o.harbar@studenti.unisa.it>
2023-09-29 08:42:35 +0200

    commit versione non completata

--- src/main/java/org/example/Calcolatrice.java
+++ src/main/java/org/example/Calcolatrice.java
@@ -1,2 +1,14 @@
-package org.example;public class Calcolatrice {
+package org.example;
+
+public class Calcolatrice {
+    public Calcolatrice(){
+    }
+
+    public static float add(int a, int b){
+        return a+b;
+    }
+
+    public static float minus(int a, int b){
+        return a-b;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,16 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner in = new Scanner(System.in);
         System.out.println("Gutentag");
+        System.out.println("Inserisci due cifre da processare\ninserisci A:\n");
+        float a = in.nextFloat();
+        System.out.println("inserisci B:\n");
+
+
+
     }
 }
